ec47670955fc4f0fb65e44566a17c8ba09ca5468
Predakor <patrykbusko@gmail.com>
2022-03-11 23:57:22 +0100

    start of tiling system tool

---
+++ MonopolyCopy/Assets/UpoPoly/Scripts/Tools/BoardGenerator.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGenerator : MonoBehaviour
+{
+    int size = 32;
+    int verticies = 4;
+
+    [SerializeField]
+    GameObject tile;
+
+    void Awake()
+    {
+        Generate();
+    }
+    void Start()
+    {
+
+    }
+
+    void Generate()
+    {
+        int rowSize = size / verticies;
+
+        Vector3 position = transform.position;
+        int offset = 1;
+
+        for (int i = 0; i < rowSize; i++)
+        {
+            position.x -= offset;
+            Instantiate(tile, position, transform.rotation, parent: transform);
+        }
+        for (int i = 0; i < rowSize; i++)
+        {
+            position.z -= offset;
+            Instantiate(tile, position, transform.rotation, parent: transform);
+        }
+        for (int i = 0; i < rowSize; i++)
+        {
+            position.x += offset;
+            Instantiate(tile, position, transform.rotation, parent: transform);
+        }
+        for (int i = 0; i < rowSize; i++)
+        {
+            position.z += offset;
+            Instantiate(tile, position, transform.rotation, parent: transform);
+        }
+    }
+}
