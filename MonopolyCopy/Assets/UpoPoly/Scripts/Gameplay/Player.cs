5c057cc8d86f7d2633886c8de8019bdf53c6c82a
Predakor <patrykbusko@gmail.com>
2022-03-30 16:06:41 +0200

    changed to photon server

--- MonopolyCopy/Assets/UpoPoly/Scripts/Gameplay/Player.cs
+++ MonopolyCopy/Assets/UpoPoly/Scripts/Gameplay/Player.cs
@@ -3,11 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using Unity.Netcode;
 
-public class Player : NetworkBehaviour
+public class Player : MonoBehaviour
 {
-
     [SerializeField] new string name;
     [SerializeField] int money = 1000;
     [SerializeField] List<Tile> ownedTiles;
@@ -27,7 +25,6 @@ public class Player : NetworkBehaviour
 
     void Update()
     {
-        transform.position = Position.Value;
     }
 
     public void AddMoney(int amount)
@@ -62,39 +59,4 @@ public class Player : NetworkBehaviour
         moneyDisplay.text = $"{money}$";
         nameDisplay.text = name;
     }
-    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
-
-    public override void OnNetworkSpawn()
-    {
-        if (IsOwner)
-        {
-            Move();
-        }
-    }
-
-    public void Move()
-    {
-        if (NetworkManager.Singleton.IsServer)
-        {
-            var randomPosition = GetRandomPositionOnPlane();
-            transform.position = randomPosition;
-            Position.Value = randomPosition;
-        }
-        else
-        {
-            SubmitPositionRequestServerRpc();
-        }
-    }
-
-    [ServerRpc]
-    void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
-    {
-        Position.Value = GetRandomPositionOnPlane();
-    }
-
-    static Vector3 GetRandomPositionOnPlane()
-    {
-        return new Vector3(UnityEngine.Random.Range(-3f, 3f), 1f, UnityEngine.Random.Range(-3f, 3f));
-    }
-
 }
